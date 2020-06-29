a539d0644582b877e48cbcab6b063639315770e0
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-29 09:13:33 +0200

    Unit test stub for app-namespaces

---
+++ test/cogset_common/app_namespaces_test.clj
@@ -0,0 +1,34 @@
+(ns cogset-common.app-namespaces-test
+  (:require [clojure.test :refer :all]
+            [cogset-common.app-namespaces :refer :all]))
+
+;
+; Common functions used by tests.
+;
+
+(defn callable?
+    "Test if given function-name is bound to the real function."
+    [function-name]
+    (clojure.test/function? function-name))
+
+;
+; Tests for function existence
+;
+
+(deftest test-get-app-namespaces-existence
+    "Check that the cogset-common.app-namespaces/get-app-namespaces definition exists."
+    (testing "if the cogset-common.app-namespaces/get-app-namespaces definition exists."
+        (is (callable? 'cogset-common.app-namespaces/get-app-namespaces))))
+
+
+(deftest test-load-selected-namespace-existence
+    "Check that the cogset-common.app-namespaces/load-selected-namespace definition exists."
+    (testing "if the cogset-common.app-namespaces/load-selected-namespace definition exists."
+        (is (callable? 'cogset-common.app-namespaces/load-selected-namespace))))
+
+
+(deftest test-load-app-namespaces-existence
+    "Check that the cogset-common.app-namespaces/load-app-namespaces definition exists."
+    (testing "if the cogset-common.app-namespaces/load-app-namespaces definition exists."
+        (is (callable? 'cogset-common.app-namespaces/load-app-namespaces))))
+
