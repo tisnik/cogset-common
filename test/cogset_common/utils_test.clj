1549de3b5f88d51f43b4d2ffb1ad7bb92c963751
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-29 09:14:09 +0200

    Unit test stub for utils

---
+++ test/cogset_common/utils_test.clj
@@ -0,0 +1,28 @@
+(ns cogset-common.utils-test
+  (:require [clojure.test :refer :all]
+            [cogset-common.utils :refer :all]))
+
+;
+; Common functions used by tests.
+;
+
+(defn callable?
+    "Test if given function-name is bound to the real function."
+    [function-name]
+    (clojure.test/function? function-name))
+
+;
+; Tests for function existence
+;
+
+(deftest test-third-existence
+    "Check that the cogset-common.utils/third definition exists."
+    (testing "if the cogset-common.utils/third definition exists."
+        (is (callable? 'cogset-common.utils/third))))
+
+
+(deftest test-api-endpoint?-existence
+    "Check that the cogset-common.utils/api-endpoint? definition exists."
+    (testing "if the cogset-common.utils/api-endpoint? definition exists."
+        (is (callable? 'cogset-common.utils/api-endpoint?))))
+
