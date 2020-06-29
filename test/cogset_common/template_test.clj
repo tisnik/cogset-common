2ed3f048ab5892def474c9bdf418e8b021a44751
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-29 09:14:04 +0200

    Unit test stub for template

---
+++ test/cogset_common/template_test.clj
@@ -0,0 +1,28 @@
+(ns cogset-common.template-test
+  (:require [clojure.test :refer :all]
+            [cogset-common.template :refer :all]))
+
+;
+; Common functions used by tests.
+;
+
+(defn callable?
+    "Test if given function-name is bound to the real function."
+    [function-name]
+    (clojure.test/function? function-name))
+
+;
+; Tests for function existence
+;
+
+(deftest test-load-template-existence
+    "Check that the cogset-common.template/load-template definition exists."
+    (testing "if the cogset-common.template/load-template definition exists."
+        (is (callable? 'cogset-common.template/load-template))))
+
+
+(deftest test-eval-template-existence
+    "Check that the cogset-common.template/eval-template definition exists."
+    (testing "if the cogset-common.template/eval-template definition exists."
+        (is (callable? 'cogset-common.template/eval-template))))
+
