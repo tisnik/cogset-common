3e94e326782cf268fa3d325daf437ba79aa9de9c
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-30 08:30:11 +0200

    Reformatted io.clj by zprint

--- src/cogset_common/io.clj
+++ src/cogset_common/io.clj
@@ -15,26 +15,24 @@
 (require '[clojure.tools.logging :as log])
 
 (defn slurp-
-    [filename]
-    (try
-        (slurp filename)
-        (catch Exception e
-            (log/error (.getMessage e))
-            nil)))
+  [filename]
+  (try (slurp filename)
+       (catch Exception e
+             (log/error (.getMessage e))
+             nil)))
 
 (defn load-text
-    [filename]
-    (try
-        (-> (slurp filename)
-            (clojure.string/split-lines))
-        (catch Exception e
-            (log/error (.getMessage e))
-            nil)))
+  [filename]
+  (try (-> (slurp filename)
+           (clojure.string/split-lines))
+       (catch Exception e
+             (log/error (.getMessage e))
+             nil)))
 
 (defn load-and-parse-text
-    [filename parse-function]
-    (if-let [lines (load-text filename)]
-             (into {}
-                 (for [line lines]
-                     (parse-function line)))))
+  [filename parse-function]
+  (if-let [lines (load-text filename)]
+    (into {}
+          (for [line lines]
+               (parse-function line)))))
 
