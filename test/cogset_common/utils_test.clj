910c5d22c40064f265c8dd133c957197a2c24de1
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-30 08:36:54 +0200

    Reformatted utils_test.clj by zprint

--- test/cogset_common/utils_test.clj
+++ test/cogset_common/utils_test.clj
@@ -7,22 +7,22 @@
 ;
 
 (defn callable?
-    "Test if given function-name is bound to the real function."
-    [function-name]
-    (clojure.test/function? function-name))
+  "Test if given function-name is bound to the real function."
+  [function-name]
+  (clojure.test/function? function-name))
 
 ;
 ; Tests for function existence
 ;
 
 (deftest test-third-existence
-    "Check that the cogset-common.utils/third definition exists."
-    (testing "if the cogset-common.utils/third definition exists."
-        (is (callable? 'cogset-common.utils/third))))
+  "Check that the cogset-common.utils/third definition exists."
+  (testing "if the cogset-common.utils/third definition exists."
+           (is (callable? 'cogset-common.utils/third))))
 
 
 (deftest test-api-endpoint?-existence
-    "Check that the cogset-common.utils/api-endpoint? definition exists."
-    (testing "if the cogset-common.utils/api-endpoint? definition exists."
-        (is (callable? 'cogset-common.utils/api-endpoint?))))
+  "Check that the cogset-common.utils/api-endpoint? definition exists."
+  (testing "if the cogset-common.utils/api-endpoint? definition exists."
+           (is (callable? 'cogset-common.utils/api-endpoint?))))
 
