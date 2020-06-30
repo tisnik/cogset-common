7cc19485d91adee4599ed79a2d5907142f74c078
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-30 08:28:45 +0200

    Reformatted app_namespaces.clj by zprint

--- src/cogset_common/app_namespaces.clj
+++ src/cogset_common/app_namespaces.clj
@@ -15,24 +15,23 @@
 (require '[clojure.tools.logging :as log])
 
 (defn get-app-namespaces
-    [namespaces module-name]
-    (->> namespaces
-         (map #(str % "." module-name))
-         (map symbol)))
+  [namespaces module-name]
+  (->> namespaces
+       (map #(str % "." module-name))
+       (map symbol)))
 
 (defn load-selected-namespace
-    [namespace]
-    (try
-        (log/info "Loading namespace" namespace)
-        (require namespace)
-        (catch java.io.FileNotFoundException e
-            (log/error (str "Unable to load application " namespace))
-            (log/error "Please read README.md how to add app properly")
-            (System/exit 1))))
+  [namespace]
+  (try (log/info "Loading namespace" namespace)
+       (require namespace)
+       (catch java.io.FileNotFoundException e
+         (log/error (str "Unable to load application " namespace))
+         (log/error "Please read README.md how to add app properly")
+         (System/exit 1))))
 
 (defn load-app-namespaces
-    [namespace-names module-names]
-    (doseq [module-name module-names]
-        (doseq [namespace (get-app-namespaces namespace-names module-name)]
-            (load-selected-namespace namespace))))
+  [namespace-names module-names]
+  (doseq [module-name module-names]
+    (doseq [namespace (get-app-namespaces namespace-names module-name)]
+      (load-selected-namespace namespace))))
 
