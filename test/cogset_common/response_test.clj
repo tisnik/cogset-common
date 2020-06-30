5530fbb1f21122fc00594133f4a75b9fd63aad68
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-30 08:36:43 +0200

    Reformatted response_test.clj by zprint

--- test/cogset_common/response_test.clj
+++ test/cogset_common/response_test.clj
@@ -7,28 +7,29 @@
 ;
 
 (defn callable?
-    "Test if given function-name is bound to the real function."
-    [function-name]
-    (clojure.test/function? function-name))
+  "Test if given function-name is bound to the real function."
+  [function-name]
+  (clojure.test/function? function-name))
 
 ;
 ; Tests for function existence
 ;
 
 (deftest test-send-json-response-existence
-    "Check that the cogset-common.response/send-json-response definition exists."
-    (testing "if the cogset-common.response/send-json-response definition exists."
-        (is (callable? 'cogset-common.response/send-json-response))))
+  "Check that the cogset-common.response/send-json-response definition exists."
+  (testing "if the cogset-common.response/send-json-response definition exists."
+           (is (callable? 'cogset-common.response/send-json-response))))
 
 
 (deftest test-send-html-response-existence
-    "Check that the cogset-common.response/send-html-response definition exists."
-    (testing "if the cogset-common.response/send-html-response definition exists."
-        (is (callable? 'cogset-common.response/send-html-response))))
+  "Check that the cogset-common.response/send-html-response definition exists."
+  (testing "if the cogset-common.response/send-html-response definition exists."
+           (is (callable? 'cogset-common.response/send-html-response))))
 
 
 (deftest test-send-error-response-existence
-    "Check that the cogset-common.response/send-error-response definition exists."
-    (testing "if the cogset-common.response/send-error-response definition exists."
-        (is (callable? 'cogset-common.response/send-error-response))))
+  "Check that the cogset-common.response/send-error-response definition exists."
+  (testing
+    "if the cogset-common.response/send-error-response definition exists."
+    (is (callable? 'cogset-common.response/send-error-response))))
 
