134006633f0a86305d3aa2b3015efc90189f7eeb
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-30 08:36:23 +0200

    Reformatted app_namespaces_test.clj by zprint

--- test/cogset_common/app_namespaces_test.clj
+++ test/cogset_common/app_namespaces_test.clj
@@ -7,28 +7,31 @@
 ;
 
 (defn callable?
-    "Test if given function-name is bound to the real function."
-    [function-name]
-    (clojure.test/function? function-name))
+  "Test if given function-name is bound to the real function."
+  [function-name]
+  (clojure.test/function? function-name))
 
 ;
 ; Tests for function existence
 ;
 
 (deftest test-get-app-namespaces-existence
-    "Check that the cogset-common.app-namespaces/get-app-namespaces definition exists."
-    (testing "if the cogset-common.app-namespaces/get-app-namespaces definition exists."
-        (is (callable? 'cogset-common.app-namespaces/get-app-namespaces))))
+  "Check that the cogset-common.app-namespaces/get-app-namespaces definition exists."
+  (testing
+    "if the cogset-common.app-namespaces/get-app-namespaces definition exists."
+    (is (callable? 'cogset-common.app-namespaces/get-app-namespaces))))
 
 
 (deftest test-load-selected-namespace-existence
-    "Check that the cogset-common.app-namespaces/load-selected-namespace definition exists."
-    (testing "if the cogset-common.app-namespaces/load-selected-namespace definition exists."
-        (is (callable? 'cogset-common.app-namespaces/load-selected-namespace))))
+  "Check that the cogset-common.app-namespaces/load-selected-namespace definition exists."
+  (testing
+    "if the cogset-common.app-namespaces/load-selected-namespace definition exists."
+    (is (callable? 'cogset-common.app-namespaces/load-selected-namespace))))
 
 
 (deftest test-load-app-namespaces-existence
-    "Check that the cogset-common.app-namespaces/load-app-namespaces definition exists."
-    (testing "if the cogset-common.app-namespaces/load-app-namespaces definition exists."
-        (is (callable? 'cogset-common.app-namespaces/load-app-namespaces))))
+  "Check that the cogset-common.app-namespaces/load-app-namespaces definition exists."
+  (testing
+    "if the cogset-common.app-namespaces/load-app-namespaces definition exists."
+    (is (callable? 'cogset-common.app-namespaces/load-app-namespaces))))
 
