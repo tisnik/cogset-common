4e161b47c0928d8d83869f700d5f1b83663119f3
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-30 08:36:15 +0200

    Reformatted app_list_test.clj by zprint

--- test/cogset_common/app_list_test.clj
+++ test/cogset_common/app_list_test.clj
@@ -7,28 +7,28 @@
 ;
 
 (defn callable?
-    "Test if given function-name is bound to the real function."
-    [function-name]
-    (clojure.test/function? function-name))
+  "Test if given function-name is bound to the real function."
+  [function-name]
+  (clojure.test/function? function-name))
 
 ;
 ; Tests for function existence
 ;
 
 (deftest test-load-app-list-existence
-    "Check that the cogset-common.app-list/load-app-list definition exists."
-    (testing "if the cogset-common.app-list/load-app-list definition exists."
-        (is (callable? 'cogset-common.app-list/load-app-list))))
+  "Check that the cogset-common.app-list/load-app-list definition exists."
+  (testing "if the cogset-common.app-list/load-app-list definition exists."
+           (is (callable? 'cogset-common.app-list/load-app-list))))
 
 
 (deftest test-filter-app-existence
-    "Check that the cogset-common.app-list/filter-app definition exists."
-    (testing "if the cogset-common.app-list/filter-app definition exists."
-        (is (callable? 'cogset-common.app-list/filter-app))))
+  "Check that the cogset-common.app-list/filter-app definition exists."
+  (testing "if the cogset-common.app-list/filter-app definition exists."
+           (is (callable? 'cogset-common.app-list/filter-app))))
 
 
 (deftest test-read-app-list-existence
-    "Check that the cogset-common.app-list/read-app-list definition exists."
-    (testing "if the cogset-common.app-list/read-app-list definition exists."
-        (is (callable? 'cogset-common.app-list/read-app-list))))
+  "Check that the cogset-common.app-list/read-app-list definition exists."
+  (testing "if the cogset-common.app-list/read-app-list definition exists."
+           (is (callable? 'cogset-common.app-list/read-app-list))))
 
