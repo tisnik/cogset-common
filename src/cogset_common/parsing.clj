3f55ae75c7347807cc6b17574941db545ad22e79
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-30 08:30:55 +0200

    Reformatted parsing.clj by zprint

--- src/cogset_common/parsing.clj
+++ src/cogset_common/parsing.clj
@@ -15,10 +15,9 @@
 (require '[clojure.tools.logging :as log])
 
 (defn parse-int
-    [string-value]
-    (try
-        (Integer/parseInt string-value)
-        (catch Exception e
-            (log/error (.getMessage e))
-            nil)))
+  [string-value]
+  (try (Integer/parseInt string-value)
+       (catch Exception e
+              (log/error (.getMessage e))
+              nil)))
 
